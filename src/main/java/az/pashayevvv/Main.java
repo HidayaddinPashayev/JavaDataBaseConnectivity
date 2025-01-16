d4a3ff238022be127607a659a8db518f2ff80b85
HidayaddinPashayev <hpashayev18207@ada.edu.az>
2025-01-16 17:49:03 +0400

    Last commit

--- src/main/java/az/pashayevvv/Main.java
+++ src/main/java/az/pashayevvv/Main.java
@@ -1,18 +1,81 @@
 package az.pashayevvv;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        System.out.println("salamlar");
+import az.pashayevvv.dao.PatientDao;
+import az.pashayevvv.model.Patient;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PatientDao patientDao = new PatientDao();
+
+        while (true) {
+            System.out.println("What do you want? \n1. Create Patient table\n" +
+                    "2. Save Patient\n" +
+                    "3. Get All Patients\n" +
+                    "4. Get Patient by ID\n" +
+                    "5. Update Patient by ID\n" +
+                    "6. Delete Patient by ID");
+
+            Scanner scanner = new Scanner(System.in);
+            Patient patient = new Patient();
+            switch (scanner.nextInt()) {
+                case 1:
+                    patientDao.createPatient();
+                    break;
+                case 2:
+                    patientDao = new PatientDao();
+                    patientDao.insertPatient();
+                    break;
+                case 3:
+                    System.out.println(patientDao.getAllPatients());
+                    break;
+                case 4:
+                    System.out.println("Enter patient ID: ");
+                    int id = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println(patientDao.getById(id));
+                    break;
+                case 5:
+                    System.out.println("Enter patient ID to update: ");
+                    int idToUpdate = scanner.nextInt();
+                    scanner.nextLine();
+                    Patient patientt = patientDao.getById(idToUpdate);
+                    if (patientt == null) {
+                        throw new RuntimeException("Patient not found");
+                    }
+                    System.out.println(patientt);
+                    System.out.println("Enter patient name: ");
+                    String name = scanner.next();
+                    patientt.setName(name);
+                    System.out.println("Enter patient surname: ");
+                    String surname = scanner.next();
+                    patientt.setSurname(surname);
+                    System.out.println("Enter patient age: ");
+                    patientt.setAge(scanner.nextInt());
+                    scanner.nextLine();
+                    System.out.println("Enter patient gender: ");
+                    patientt.setGender(scanner.next());
+                    scanner.nextLine();
+                    System.out.println("Enter patient disease type: ");
+                    patientt.setDiseaseType(scanner.next());
+                    scanner.nextLine();
+                    patientDao.updatePatient(patientt, idToUpdate); // Replace with update method if needed
+                    break;
+                case 6:
+                    System.out.println("Enter patient ID to delete: ");
+                    id = scanner.nextInt();
+                    Patient patientToDelete = patientDao.getById(id);
+                    if (patientToDelete == null) {
+                        throw new RuntimeException("Patient not found");
+                    }
+                    patientDao.deletePatient(patientToDelete.getId()); // Replace with delete method if needed
+                    break;
+                default:
+                    System.out.println("Invalid option");
+            }
         }
+
     }
 }
